82649e07a37767a9b10699a1725f1c835d6f053a
chandraraja556 <44883125+chandraraja556@users.noreply.github.com>
2018-11-09 11:17:20 -0500

    Update DemoApplication.java

--- src/main/java/com/raja/Demo/DemoApplication.java
+++ src/main/java/com/raja/Demo/DemoApplication.java
@@ -1,10 +1,16 @@
 package com.raja.Demo;
 
+import com.raja.Demo.model.Station;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @SpringBootApplication
-public class DemoApplication {
+public class Demo {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
